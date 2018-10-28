4be971fe1c02dfda0f4e26ecbe6ec57f7b7fc30a
ANDREW <afzm4@mst.edu>
2018-10-28 16:02:17 -0500

    Updated files.

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Oct 28 13:58:23 2018
-
-@author: Andrew
+Andrew Floyd
+CS3001 - HW4 (NaiveBayesian)
+October 28th, 2018
 """
 
 import pandas as pd
 import numpy as np
-import time
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
-from sklearn.metrics import precision_recall_fscore_support
-
+from sklearn.metrics import precision_recall_fscore_support, accuracy_score
 
 data = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
@@ -43,8 +40,10 @@ y_pred = gnb.predict(test_cleaned[used_features])
 
 y_true = np.array([0,1,0,0,0,0,0,0,0,1,0,1])
 
-prec, recall, fscore, support = (precision_recall_fscore_support(y_true, y_pred, average='binary'))
+accu = accuracy_score(y_true, y_pred)
+prec, recall, fscore, support = precision_recall_fscore_support(y_true, y_pred, average="binary")
 
+print("Accuracy: ", accu)
 print("Precision: ", prec)
 print("Recall: ", recall)
 print("F1: ", fscore)
